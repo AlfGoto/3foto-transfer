8b620e2336ba95151a7daa078de3cf592a9cefb5
Alf <alfgoto@gmail.com>
2024-12-11 18:35:19 +0100

    chore: project created with projen

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,14 +1,10 @@
 import { awscdk, javascript } from 'projen';
-const project = new awscdk.AwsCdkConstructLibrary({
-  author: 'Alf',
-  authorAddress: 'alfgoto@gmail.com',
+const project = new awscdk.AwsCdkTypeScriptApp({
   cdkVersion: '2.1.0',
   defaultReleaseBranch: 'main',
-  jsiiVersion: '~5.5.0',
   name: '3foto-transfer',
   packageManager: javascript.NodePackageManager.NPM,
   projenrcTs: true,
-  repositoryUrl: 'git@github.com:AlfGoto/3foto-transfer.git',
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
